4a53ce27c4a4eb11043c42f24037cc05295e48c0
Anasty-Sia <anastasiasizonenkoo@yandex.ru>
2025-05-26 20:52:24 +0300

    Выполнено ТЗ по Практической работе №3

---
+++ src/main/kotlin/ArchiveMenu.kt
@@ -0,0 +1,28 @@
+class ArchiveMenu {
+
+    fun showArchiveMenu(archives: MutableList<Archive>) {
+        var exit = false
+        while (!exit) {
+            val menuItems = mutableMapOf<String, () -> Unit>()
+            menuItems["Создать архив"] = { createArchive(archives) }
+            for (archive in archives) {
+                menuItems[archive.name] = {
+                    NotesMenu().showNotesMenu(archive)
+                }
+            }
+            menuItems["Выход"] = {
+                exit = true
+            }
+            Menu("Список архивов:", menuItems).show()
+        }
+    }
+
+    fun createArchive(archives: MutableList<Archive>) {
+        println("Введите название архива:")
+        val name = readlnOrNull()?.takeIf { it.isNotBlank() } ?: run {
+            println("Название не может быть пустым!")
+            return
+        }
+        archives.add(Archive(name))
+    }
+}
---
+++ src/main/kotlin/Menu.kt
@@ -0,0 +1,23 @@
+class Menu(private val title: String, private val items: Map<String, () -> Unit>) {
+    fun show() {
+        var shouldContinue = true
+
+        while (shouldContinue) {
+            println(title)
+            val itemsList = items.entries.toList()
+            for ((index, item) in itemsList.withIndex()) {
+                println("$index. ${item.key}")
+            }
+            val input = readLine()
+            val choice = input?.toIntOrNull()
+            when {
+                choice == null -> println("Ошибка: нужно ввести число")
+                choice < 0 || choice >= items.size -> println("Ошибка: выберите число от 0 до ${items.size - 1}")
+                else -> {
+                    itemsList[choice].value()
+                    shouldContinue = false
+                }
+            }
+        }
+    }
+}
